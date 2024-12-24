ae65e5b3a211e81d649dd78b22f9585051be1fb3
suhwr <152080868+suhwr@users.noreply.github.com>
2024-12-24 18:03:22 +0700

    Update memegenerator.py
    - Command line
    + Color emoji
    + Output filePath

--- memegenerator.py
+++ memegenerator.py
@@ -1,147 +1,115 @@
-import re
-import os
 from PIL import Image, ImageDraw, ImageFont, ImageSequence
-import sys
+from os import path
+from re import match
 
-dir_path = os.path.dirname(os.path.abspath(__file__))
 
-def remove_emojis(text):
-    # Regular expression for matching emojis
-    emoji_pattern = re.compile(
-        '['
-        '\U0001F600-\U0001F64F'  # Emoticons
-        '\U0001F300-\U0001F5FF'  # Symbols & Pictographs
-        '\U0001F680-\U0001F6FF'  # Transport & Map Symbols
-        '\U0001F700-\U0001F77F'  # Alchemical Symbols
-        '\U0001F780-\U0001F7FF'  # Geometric Shapes Extended
-        '\U0001F800-\U0001F8FF'  # Supplemental Arrows-C
-        '\U0001F900-\U0001F9FF'  # Supplemental Symbols and Pictographs
-        '\U0001FA00-\U0001FA6F'  # Chess Symbols
-        '\U0001FA70-\U0001FAFF'  # Symbols and Pictographs Extended-A
-        '\U00002702-\U000027B0'  # Dingbats
-        '\U000024C2-\U0001F251'
-        ']+', flags=re.UNICODE)
-    return emoji_pattern.sub(r'', text).strip()
+__dirname = path.dirname(path.abspath(__file__))
 
-def get_upper(somedata):
-    '''
-    Handle Python 2/3 differences in argv encoding
-    '''
-    result = ''
-    try:
-        # Decode and remove emojis
-        cleaned_data = remove_emojis(somedata.decode("utf-8"))
-        result = cleaned_data.upper()
-    except AttributeError:  # Handle Python 3 strings
-        cleaned_data = remove_emojis(somedata)
-        result = cleaned_data.upper()
-    return result
 
-def get_lower(somedata):
-    '''
-    Handle Python 2/3 differences in argv encoding
-    '''
-    result = ''
-    try:
-        # Decode and remove emojis
-        cleaned_data = remove_emojis(somedata.decode("utf-8"))
-        result = cleaned_data.lower()
-    except AttributeError:  # Handle Python 3 strings
-        cleaned_data = remove_emojis(somedata)
-        result = cleaned_data.lower()
-    return result
+def is_emoji(char):
+  return match(
+    r'['
+    '\U0001F600-\U0001F64F'  # Emoticons
+    '\U0001F300-\U0001F5FF'  # Symbols & Pictographs
+    '\U0001F680-\U0001F6FF'  # Transport & Map Symbols
+    '\U0001F700-\U0001F77F'  # Alchemical Symbols
+    '\U0001F780-\U0001F7FF'  # Geometric Shapes Extended
+    '\U0001F800-\U0001F8FF'  # Supplemental Arrows-C
+    '\U0001F900-\U0001F9FF'  # Supplemental Symbols and Pictographs
+    '\U0001FA00-\U0001FA6F'  # Chess Symbols
+    '\U0001FA70-\U0001FAFF'  # Symbols and Pictographs Extended-A
+    '\U00002702-\U000027B0'  # Dingbats
+    '\U000024C2-\U0001F251'  # Enclosed Characters
+    ']+', char)
 
 def getsize(font, text):
-    left, top, right, bottom = font.getbbox(text)
-    return right - left, bottom - top
- 
-def make_meme(topString, bottomString, filename):
-    # Open the image
-    original = Image.open(filename)
-    
-    if original.format in ['GIF', 'WEBP']:
-        frames = []
-        for frame in ImageSequence.Iterator(original):
-            img = frame.copy().convert("RGBA")
-            process_frame(img, topString, bottomString)
-            frames.append(img)
-        
-        if original.format == 'WEBP':
-            frames[0].save("temp.webp", save_all=True, append_images=frames[1:], loop=0, duration=original.info['duration'])
-            print("file save in temp.webp")
-        else:
-            frames[0].save("temp.gif", save_all=True, append_images=frames[1:], loop=0)
-            print("file save in temp.gif")
-    else:
-        img = original
-        process_frame(img, topString, bottomString)
-        img.save("temp." + )
-        print(f'file save in temp.{original.format.lower()}')
+  left, top, right, bottom = font.getbbox(text)
+  return right - left, bottom - top
+
 
 def process_frame(img, topString, bottomString):
-    imageSize = img.size
+  imageSize = img.size
+  fontPath = path.join(__dirname, 'Fonts', 'impact.ttf')
+  emojiFontPath = path.join(__dirname, 'Fonts', 'NotoColorEmoji.ttf')
 
-    # Find the biggest font size that works
-    fontSize = int(imageSize[1] / 7.5)
-    font = ImageFont.truetype(f"{dir_path}/Fonts/impact.ttf", fontSize)
-    topTextSize = getsize(font, topString)
-    bottomTextSize = getsize(font, bottomString)
-    while topTextSize[0] > imageSize[0] - 20 or bottomTextSize[0] > imageSize[0] - 20:
-        fontSize -= 1
-        font = ImageFont.truetype(f"{dir_path}/Fonts/impact.ttf", fontSize)
-        topTextSize = getsize(font, topString)
-        bottomTextSize = getsize(font, bottomString)
+  fontSize = int(imageSize[1] / 7.5)
+  font = ImageFont.truetype(fontPath, fontSize)
+  emojiFont = ImageFont.truetype(emojiFontPath, 109)
 
-    # Find top centered position for top text
-    topTextPositionX = (imageSize[0] / 2) - (topTextSize[0] / 2)
-    topTextPositionY = 0
-    topTextPosition = (topTextPositionX, topTextPositionY)
+  def calculate_text_size(text):
+    width, height = 0, 0
+    for char in text:
+      char_width, char_height = getsize(font, char)
+      width += char_width
+      height = max(height, char_height)
+    return width, height
 
-    # Find bottom centered position for bottom text
-    bottomTextPositionX = (imageSize[0] / 2) - (bottomTextSize[0] / 2)
-    padding = imageSize[1] * 0.07
-    bottomTextPositionY = imageSize[1] - bottomTextSize[1] - padding
-    bottomTextPosition = (bottomTextPositionX, bottomTextPositionY)
+  topTextSize = calculate_text_size(topString)
+  bottomTextSize = calculate_text_size(bottomString)
+  while topTextSize[0] > imageSize[0] - 20 or bottomTextSize[0] > imageSize[0] - 20:
+    fontSize -= 1
+    font = ImageFont.truetype(fontPath, fontSize)
+    topTextSize = calculate_text_size(topString)
+    bottomTextSize = calculate_text_size(bottomString)
 
-    draw = ImageDraw.Draw(img)
+  topTextPosition = ((imageSize[0] - topTextSize[0]) / 2, 8)
+  bottomTextPosition = ((imageSize[0] - bottomTextSize[0]) / 2, imageSize[1] - bottomTextSize[1] - imageSize[1] * 0.07)
 
-    # Draw outlines
-    outlineRange = int(fontSize / 15)
-    for x in range(-outlineRange, outlineRange + 1):
-        for y in range(-outlineRange, outlineRange + 1):
-            draw.text((topTextPosition[0] + x, topTextPosition[1] + y), topString, (0, 0, 0), font=font)
-            draw.text((bottomTextPosition[0] + x, bottomTextPosition[1] + y), bottomString, (0, 0, 0), font=font)
+  draw = ImageDraw.Draw(img)
+  outlineRange = int(fontSize / 20)
 
-    draw.text(topTextPosition, topString, (255, 255, 255), font=font)
-    draw.text(bottomTextPosition, bottomString, (255, 255, 255), font=font)
+  def draw_text_with_outline(draw, position, text):
+    x, y = position
+    cx, cy = x, y
+
+    for char in text:
+      if not is_emoji(char):
+        for ox in range(-outlineRange, outlineRange + 1):
+          for oy in range(-outlineRange, outlineRange + 1):
+            draw.text((cx + ox, cy + oy), char, fill=(0, 0, 0), font=font)
+        cx += getsize(font, char)[0]
+
+    cx, cy = x, y
+    for char in text:
+      if is_emoji(char):
+        current_font = emojiFont
+        char_width, char_height = getsize(current_font, char)
+        emoji_image = Image.new("RGBA", (char_width, char_height))
+        emoji_draw = ImageDraw.Draw(emoji_image)
+        emoji_draw.text((0, 0), char, font=current_font, fill=(255, 255, 255), embedded_color=True)
+        emoji_image = emoji_image.resize((fontSize, fontSize), Image.Resampling.LANCZOS)
+        img.paste(emoji_image, (int(cx), int(cy) + 5), emoji_image)
+        cx += fontSize
+      else:
+        draw.text((cx, cy), char, fill=(255, 255, 255), font=font)
+        cx += getsize(font, char)[0]
+  
+  draw_text_with_outline(draw, topTextPosition, topString)
+  draw_text_with_outline(draw, bottomTextPosition, bottomString)
+
+def memegenerator(inputPath, topString = "", bottomString = ""):
+  topString = topString.upper()
+  bottomString = bottomString.upper()
+  original = Image.open(inputPath)
+  outputPath = path.join(__dirname, f"meme_{path.basename(inputPath)}")
+  
+  if original.format in ['GIF', 'WEBP']:
+    frames = []
+    for frame in ImageSequence.Iterator(original):
+      img = frame.copy().convert("RGBA")
+      process_frame(img, topString, bottomString)
+      frames.append(img)
+    
+    if original.format == 'WEBP':
+      frames[0].save(outputPath, save_all=True, append_images=frames[1:], loop=0, duration=original.info['duration'])
+    else:
+      frames[0].save(outputPath, save_all=True, append_images=frames[1:], loop=0)
+  else:
+    img = original
+    process_frame(img, topString, bottomString)
+    img.save(outputPath)
+  
+  return outputPath
 
 if __name__ == '__main__':
-    args_len = len(sys.argv)
-    topString = ''
-    meme = 'standard.jpg'
-
-    if args_len == 1:
-        # No args except the launch of the script
-        print('args plz')
-
-    elif args_len == 2:
-        # Only one argument, use standard meme
-        bottomString = get_upper(sys.argv[-1])
-
-    elif args_len == 3:
-        # Args give meme and one line
-        bottomString = get_upper(sys.argv[-1])
-        meme = sys.argv[1]
-
-    elif args_len == 4:
-        # Args give meme and two lines
-        topString = get_upper(sys.argv[-2])
-        bottomString = get_upper(sys.argv[-1])
-        meme = sys.argv[1]
-    else:
-        # Too many args
-        print('too many args')
-    
-    filename = str(meme)
-    make_meme(topString, bottomString, filename)
-    
+  print(memegenerator("successkid.jpg","top text 😁"))
